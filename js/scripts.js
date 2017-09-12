9c571863dea50b381a732a2f4b8efc68d2bc247b
Adam Titus and Jessie Waite <adam@>
2017-09-12 10:15:56 -0700

    Add base html java and css code.

---
+++ js/scripts.js
@@ -0,0 +1,160 @@
+$(document).ready(function() {
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  -----
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  ------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  -------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  -------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  -------
+  });
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  $("#js").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  });
+  $("#jsdescr").click(function() {
+    $("#jsdescr").fadeToggle();
+    $("#js").fadeToggle();
+  ---------
+  });
+});
